efc1929354b7a1de9160cd2f5ea17dc856c3945a
oitsjustjose <stovallj1995@gmail.com>
2018-05-07 10:37:57 -0400

    --

--- my-app.js
+++ my-app.js
@@ -2,12 +2,12 @@ function initPage() {
     var center = new google.maps.LatLng(35.0479, -85.2960);
     var options =
         {
-            zoom: 20,
+            zoom: 10,
             center: center
         };
     var map = new google.maps.Map(document.getElementById("map"), options);
     var request = new XMLHttpRequest();
-    request.open("GET", "https://raw.githubusercontent.com/oitsjustjose/SCAL_USIgnite-911/gh-pages/pos.txt?token=ADtQ3Iw-5GZqC6zu0G6gpmiLc0OlFt6Mks5a-ZnKwA%3D%3D", true);
+    request.open("GET", "https://raw.githubusercontent.com/oitsjustjose/SCAL_USIgnite-911/gh-pages/pos.txt", true);
     request.send(null);
     request.onreadystatechange = function () {
         if (request.readyState === 4 && request.status === 200) {
@@ -17,14 +17,23 @@ function initPage() {
                 for (var i in spl) {
                     var lat = spl[i].split(", ")[0];
                     var lon = spl[i].split(", ")[1];
-                    console.log(lat, lon);
-                    new google.maps.Marker(
+                    var infowindow = new google.maps.InfoWindow({
+                        content: spl[i].split(", ")[2]
+                    });
+
+                    // console.log(lat, lon);
+                    var marker = new google.maps.Marker(
                         {
                             position: new google.maps.LatLng(lat, lon),
+                            animation: google.maps.Animation.DROP,
                             map: map,
-                            title: lat + ", " + lon
+                            label: lat + ", " + lon
                         }
-                    )
+                    );
+                    marker.addListener('click', function() {
+                        infowindow.open(map, marker);
+                    });
+
                 }
             }
         }
