b24c43e07f86ae0d646f12b0bad134aee7822d01
oitsjustjose <stovallj1995@gmail.com>
2018-05-18 14:38:26 -0500

    Progress...

--- plot.py
+++ plot.py
@@ -5,9 +5,7 @@ import pandas
 if __name__ == "__main__":
 	mplt.register_api_key('AIzaSyBVSgGHRDKMK5y3n0nCCfju5C5Xo4k6BN8')
 	data_frame = pandas.read_csv("./Call_Information.csv", index_col=0, parse_dates=True)
-
-
-
-	data_frame[data_frame.columns[0]] = pandas.to_numeric(data_frame[data_frame.columns[0]], errors="coerce")
-	data_frame[data_frame.columns[1]] = pandas.to_numeric(data_frame[data_frame.columns[1]], errors="coerce")
+	for index, val in enumerate(data_frame.copy().values):
+		val[0] = float(val[0])
+		val[1] = float(val[1])
 	mplt.density_plot(data_frame[data_frame.columns[0]], data_frame[data_frame.columns[1]])
