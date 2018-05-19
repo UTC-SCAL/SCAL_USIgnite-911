6ba1b154bda874363859b056ff8097b5e19f1ed9
oitsjustjose <stovallj1995@gmail.com>
2018-05-19 00:48:24 -0500

    working...

--- plot.py
+++ plot.py
@@ -1,11 +1,21 @@
 import matplotlib as plt
 from mapsplotlib import mapsplot as mplt
 import pandas
+import re
+
 
 if __name__ == "__main__":
-	mplt.register_api_key('AIzaSyBVSgGHRDKMK5y3n0nCCfju5C5Xo4k6BN8')
-	data_frame = pandas.read_csv("./Call_Information.csv", index_col=0, parse_dates=True)
+	mplt.register_api_key('AIzaSyBXBus27V_niwl70utmPD75puZTauyR0Ic')
+	with open ('./Call_Information.csv', 'r' ) as f:
+		content = f.read()
+	content_new = re.sub(r'.*\.,.*\n', '', content, flags = re.M)
+	output = open('./Call_Information_No_Empties.csv', 'w')
+	output.write(content_new)
+	output.close()
+	data_frame = pandas.read_csv('./Call_Information_No_Empties.csv', index_col=0, parse_dates=True)
 	for index, val in enumerate(data_frame.copy().values):
 		val[0] = float(val[0])
 		val[1] = float(val[1])
-	mplt.density_plot(data_frame[data_frame.columns[0]], data_frame[data_frame.columns[1]])
+	data_frame.columns = ['latitude', 'longitude', 'date', 'time', 'type', 'hour']
+	mplt.scatter(data_frame['latitude'], data_frame['longitude'], data_frame['hour'])
+
