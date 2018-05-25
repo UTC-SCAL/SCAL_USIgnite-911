9befd309d2879897022371ceb9b0ae6730a81bdd
oitsjustjose <stovallj1995@gmail.com>
2018-05-25 07:40:28 -0700

    Multiple choices permitted, update UI

--- my-app.js
+++ my-app.js
@@ -9,88 +9,49 @@ var icons = [
     "https://raw.githubusercontent.com/oitsjustjose/SCAL_USIgnite-911/gh-pages/icons/sat.png",
     "https://raw.githubusercontent.com/oitsjustjose/SCAL_USIgnite-911/gh-pages/icons/sun.png"
 ];
+var daysAllowed = {
+    "Monday": false,
+    "Tuesday": false,
+    "Wednesday": false,
+    "Thursday": false,
+    "Friday": false,
+    "Saturday": false,
+    "Sunday": false
+};
 
-function filter_type(criteria){
+var typesAllowed = {
+    "Unknown Injuries": false,
+    "No Injuries": false,
+    "Injuries": false,
+    "Delayed": false,
+    "Entrapment": false
+};
 
-    var request = new XMLHttpRequest();
-    request.open("GET", "https://gist.githubusercontent.com/oitsjustjose/278800f898380a8212bb6b78919c0833/raw/64be83d6a5d00d57ab8595e3e61c15242b124c51/data.csv", true);
-    request.send(null);
-    for (var i in markers) {
-        markers[i].setMap(null);
+function toggle_type(element, criteria) {
+    if (!typesAllowed[criteria]) {
+        typesAllowed[criteria] = true;
+        element.innerHTML += "<i class='ui right checkmark icon'></i>"
     }
-    markers = [];
-    request.onreadystatechange = function () {
-        if (request.readyState === 4 && request.status === 200) {
-            var type = request.getResponseHeader('Content-Type');
-            if (type.indexOf("text") !== 1) {
-                var spl = request.responseText.split("\n");
-                for (var i in spl) {
-                    // Ignore lines that are empty:
-                    if (spl[i].indexOf(",") === -1) {
-                        continue;
-                    }
-                    if (criteria !== 'All') {
-                        if (spl[i].split(",")[6].indexOf(criteria) === -1) {
-                            continue;
-                        }
-                    }
-                    // console.log(spl[i].split(",")[6]);
-                    var lat = spl[i].split(",")[0];
-                    var lon = spl[i].split(",")[1];
-                    var index = getIndex(spl[i].split(",")[2]);
-                    markers[i] = new google.maps.Marker(
-                        {
-                            position: new google.maps.LatLng(lat, lon),
-                            icon: icons[index],
-                            map: map
-                        }
-                    );
-                }
-            }
-        }
-    };
+    else {
+        typesAllowed[criteria] = false;
+        element.innerHTML = criteria;
+    }
+    console.log(typesAllowed);
+    updateMapMarkers();
 }
 
-function filter_day(criteria) {
-    var request = new XMLHttpRequest();
-    request.open("GET", "https://gist.githubusercontent.com/oitsjustjose/278800f898380a8212bb6b78919c0833/raw/64be83d6a5d00d57ab8595e3e61c15242b124c51/data.csv", true);
-    request.send(null);
-    for (var i in markers) {
-        markers[i].setMap(null);
+function toggle_day(element, criteria) {
+    // Indicates that there IS an icon:
+    if (!daysAllowed[criteria]) {
+        daysAllowed[criteria] = true;
+        element.innerHTML += "<i class='ui right checkmark icon'></i>"
     }
-    markers = [];
-    request.onreadystatechange = function () {
-        if (request.readyState === 4 && request.status === 200) {
-            var type = request.getResponseHeader('Content-Type');
-            if (type.indexOf("text") !== 1) {
-                var spl = request.responseText.split("\n");
-                for (var i in spl) {
-                    // Ignore lines that are empty:
-                    if (spl[i].indexOf(",") === -1) {
-                        continue;
-                    }
-                    if (criteria !== 'All') {
-                        if (spl[i].split(",")[2].indexOf(criteria) === -1) {
-                            continue;
-                        }
-                    }
-                    // console.log(spl[i].split(",")[6]);
-                    var lat = spl[i].split(",")[0];
-                    var lon = spl[i].split(",")[1];
-                    var index = getIndex(spl[i].split(",")[2]);
-                    markers[i] = new google.maps.Marker(
-                        {
-                            position: new google.maps.LatLng(lat, lon),
-                            icon: icons[index],
-                            map: map
-                        }
-                    );
-                }
-            }
-        }
-    };
+    else {
+        daysAllowed[criteria] = false;
+        element.innerHTML = criteria;
+    }
+    updateMapMarkers();
 }
-
 
 function initPage() {
     var center = new google.maps.LatLng(35.0479, -85.2960);
@@ -100,6 +61,13 @@ function initPage() {
             center: center
         };
     map = new google.maps.Map(document.getElementById("map"), options);
+}
+
+function updateMapMarkers() {
+    for (var i in markers) {
+        markers[i].setMap(null);
+    }
+    markers = [];
     var request = new XMLHttpRequest();
     request.open("GET", "https://gist.githubusercontent.com/oitsjustjose/278800f898380a8212bb6b78919c0833/raw/64be83d6a5d00d57ab8595e3e61c15242b124c51/data.csv", true);
     request.send(null);
@@ -113,9 +81,19 @@ function initPage() {
                     if (spl[i].indexOf(",") === -1) {
                         continue;
                     }
+                    var day = spl[i].split(",")[2].substring(1);
+                    var reportType = spl[i].split(",")[6].substring(1);
+                    if (!daysAllowed[day]) {
+                        continue;
+                    }
+                    if (!typesAllowed[reportType]) {
+                        console.log("Skipping type " + reportType);
+                        continue;
+                    }
+
                     var lat = spl[i].split(",")[0];
                     var lon = spl[i].split(",")[1];
-                    var index = getIndex(spl[i].split(",")[2]);
+                    var index = getIndex(day);
                     markers[i] = new google.maps.Marker(
                         {
                             position: new google.maps.LatLng(lat, lon),
