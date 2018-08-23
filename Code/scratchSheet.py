6270a58af62fad5538f909d4289e39d2ae4eb211
JeremiahRoland <fpf852@mocs.utc.edu>
2018-08-23 16:05:33 -0400

    Changes made

--- Code/scratchSheet.py
+++ Code/scratchSheet.py
@@ -1,5 +1,6 @@
 import pandas
-from datetime import datetime
+from datetime import datetime, date
+import math
 
 
 def save_excel_file(save_file_name, sheet, data_file_name):
@@ -14,46 +15,46 @@ calldata = pandas.read_excel("/home/admin/PycharmProjects/RolandProjects/Excel &
 
 # A relative temperature variable, the deviation of the mean daily temp from the monthly temp
 # This cancels out the seasonal effects
-calldata.Date = calldata.Date.astype(str)
-
-daily_temps = []
-monthly_avg_temps = []
-daily_avg_temp = 0
-count = 0
-day_num = 1
-month_num = 1
-
-for j, value in enumerate(calldata.values[0:25947]):  # covering 2017 only
-    doa = calldata.Date.values[j]
-    # print("Date is: ", doa)
-    yoa = int(doa.split('-')[0])
-    # print("Year is: ", yoa)
-    moa = int(doa.split('-')[1])
-    # print("Month is: ", moa)
-    dayoa = int(doa.split('-')[2])
-    # print("Day is: ", dayoa)
-
-    if moa == month_num:
-        if dayoa == day_num:
-            daily_avg_temp += calldata.Temperature.values[j]
-            count = count + 1
-        else:
-            daily_avg_temp = round(daily_avg_temp / count, 2)
-            daily_temps.append(daily_avg_temp)
-            daily_avg_temp = 0
-            count = 0
-            day_num = day_num + 1
-    else:
-        # print("End of month ", month_num, " reached")
-        monthly_avg = sum(daily_temps)
-        monthly_avg = round(monthly_avg / (day_num - 1), 2)
-        monthly_avg_temps.append(monthly_avg)
-
-        month_num = month_num + 1
-        # print("moa = ", moa)
-        # print("yoa = ", yoa)
-        day_num = 1
-        daily_avg_temp = 0
-        monthly_avg = 0
-        daily_temps = []
-print("Monthly Averages: ", monthly_avg_temps)
+# calldata.Date = calldata.Date.astype(str)
+#
+# daily_temps = []
+# monthly_avg_temps = []
+# daily_avg_temp = 0
+# count = 0
+# day_num = 1
+# month_num = 1
+#
+# for j, value in enumerate(calldata.values[0:25947]):  # covering 2017 only
+#     doa = calldata.Date.values[j]
+#     # print("Date is: ", doa)
+#     yoa = int(doa.split('-')[0])
+#     # print("Year is: ", yoa)
+#     moa = int(doa.split('-')[1])
+#     # print("Month is: ", moa)
+#     dayoa = int(doa.split('-')[2])
+#     # print("Day is: ", dayoa)
+#
+#     if moa == month_num:
+#         if dayoa == day_num:
+#             daily_avg_temp += calldata.Temperature.values[j]
+#             count = count + 1
+#         else:
+#             daily_avg_temp = round(daily_avg_temp / count, 2)
+#             daily_temps.append(daily_avg_temp)
+#             daily_avg_temp = 0
+#             count = 0
+#             day_num = day_num + 1
+#     else:
+#         # print("End of month ", month_num, " reached")
+#         monthly_avg = sum(daily_temps)
+#         monthly_avg = round(monthly_avg / (day_num - 1), 2)
+#         monthly_avg_temps.append(monthly_avg)
+#
+#         month_num = month_num + 1
+#         # print("moa = ", moa)
+#         # print("yoa = ", yoa)
+#         day_num = 1
+#         daily_avg_temp = 0
+#         monthly_avg = 0
+#         daily_temps = []
+# print("Monthly Averages: ", monthly_avg_temps)
