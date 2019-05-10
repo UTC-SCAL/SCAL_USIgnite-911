58fe7725cb08167f8bbb0942e219132fe4231165
JeremiahRoland <roland 101>
2019-05-10 18:05:44 -0400

    Just matched grid blocks and calldata

--- Code/gridMap.py
+++ Code/gridMap.py
@@ -2,29 +2,9 @@ import gmplot
 import pandas
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
-from math import radians, cos, sin, asin, sqrt
-from descartes.patch import PolygonPatch
-import numpy
-import math
 import os, sys
 
 path = os.path.dirname(sys.argv[0])
-
-
-# Haversine Formula #
-def haversine(long1, lat1, long2, lat2):
-    # convert decimal degrees to radians
-    long1, lat1, long2, lat2 = map(radians, [long1, lat1, long2, lat2])
-
-    # haversine formula
-    dlong = long2 - long1
-    dlat = lat2 - lat1
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlong/2)**2
-    c = 2 * asin(sqrt(a))
-    r = 3956 # the radius of the earth in miles
-    return c * r
-
-
 # Place map
 gmap = gmplot.GoogleMapPlotter(35.14, -85.17, 11)
 
@@ -45,7 +25,7 @@ gridLayout_Coords = []
 # Drawing the grid blocks on a gmap
 for i, value in enumerate(gridCoords.values):
     if i % 5 == 0:
-        # Drawing the block grid
+        # Drawing the block grid, all this does is place the grid on the map
         # We only draw on each 5th iteration, since each grid block's coordinates are grouped by 5
         # grid_lats, grid_longs = zip(*[(gridCoords.Y.values[i], gridCoords.X.values[i]),
         #                                     (gridCoords.Y.values[i+1], gridCoords.X.values[i+1]),
@@ -54,7 +34,7 @@ for i, value in enumerate(gridCoords.values):
         #                                     (gridCoords.Y.values[i+4], gridCoords.X.values[i+4])])
         # gmap.plot(grid_lats, grid_longs, 'cornflowerblue', edge_width=5)
 
-        # Making the actual polygon using the coordinates above
+        # Making the actual polygon using the coordinates above for calculations and manipulation
         poly_coords = ((gridCoords.Y.values[i], gridCoords.X.values[i]),
                                                 (gridCoords.Y.values[i+1], gridCoords.X.values[i+1]),
                                                 (gridCoords.Y.values[i+2], gridCoords.X.values[i+2]),
@@ -65,14 +45,14 @@ for i, value in enumerate(gridCoords.values):
 
 calldata = pandas.read_csv("../Excel & CSV Sheets/Full Data Grid.csv", sep=",")
 for j in range(0, len(gridLayout_Coords)):
-    poly = Polygon(gridLayout_Coords[j])
+    poly = Polygon(gridLayout_Coords[j])  # This is the grid block for the current iteration
     print(j)
     # take in the 911 incident lat and long one at a time
     for o, value2 in enumerate(calldata.values):
         call_lat = calldata.Latitude.values[o]
         call_long = calldata.Longitude.values[o]
         call_incident = Point(call_lat, call_long)
-        # See if the 911 incident is in the current polygon (representing a weather station)
+        # See if the 911 incident is in the current grid block polygon
         if poly.contains(call_incident):
             calldata.Grid_Block.values[o] = j
         else:
