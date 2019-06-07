c32de8124113b5c1daac85242d3cf298fe240fa4
JeremiahRoland <roland 101>
2019-06-07 13:20:09 -0400

    Prepping for data collection

--- Code/gridMap.py
+++ Code/gridMap.py
@@ -44,7 +44,8 @@ path = os.path.dirname(sys.argv[0])
 # Place map
 gmap = gmplot.GoogleMapPlotter(35.14, -85.17, 11)
 
-gridCoords = pandas.read_csv("../Excel & CSV Sheets/Grid Layout Test Files/VerticesPoints.csv", sep=",")
+# This file contains the coordinates for the grid blocks
+gridCoords = pandas.read_csv("../Excel & CSV Sheets/Grid Oriented Small Layout Test Files/Vertices OS Layout.csv", sep=",")
 # print(gridCoords.head())
 
 # This will hold the polygon versions of our grid blocks
@@ -63,21 +64,30 @@ for i, value in enumerate(gridCoords.values):
     if i % 5 == 0:
         # Drawing the block grid, all this does is place the grid on the map #
         # We only draw on each 5th iteration, since each grid block's coordinates are grouped by 5
-        grid_lats, grid_longs = zip(*[(gridCoords.Y.values[i], gridCoords.X.values[i]),
-                                      (gridCoords.Y.values[i+1], gridCoords.X.values[i+1]),
-                                      (gridCoords.Y.values[i+2], gridCoords.X.values[i+2]),
-                                      (gridCoords.Y.values[i+3], gridCoords.X.values[i+3]),
-                                      (gridCoords.Y.values[i+4], gridCoords.X.values[i+4])])
-        gmap.plot(grid_lats, grid_longs, 'cornflowerblue', edge_width=5)
+        # grid_lats, grid_longs = zip(*[(gridCoords.Y.values[i], gridCoords.X.values[i]),
+        #                               (gridCoords.Y.values[i+1], gridCoords.X.values[i+1]),
+        #                               (gridCoords.Y.values[i+2], gridCoords.X.values[i+2]),
+        #                               (gridCoords.Y.values[i+3], gridCoords.X.values[i+3]),
+        #                               (gridCoords.Y.values[i+4], gridCoords.X.values[i+4])])
+        # gmap.plot(grid_lats, grid_longs, 'cornflowerblue', edge_width=5)
         # Making the actual polygon using the coordinates above for calculations and manipulation #
         # poly_coords are the current polygon's coordinates (lat & long)
-        # poly_coords = ((gridCoords.Y.values[i], gridCoords.X.values[i]),
-        #                (gridCoords.Y.values[i+1], gridCoords.X.values[i+1]),
-        #                (gridCoords.Y.values[i+2], gridCoords.X.values[i+2]),
-        #                (gridCoords.Y.values[i+3], gridCoords.X.values[i+3]),
-        #                (gridCoords.Y.values[i+4], gridCoords.X.values[i+4]))
-        # # Append the poly_coords to a list for later use
-        # gridLayout_Coords.append(poly_coords)
+        poly_coords = ((gridCoords.Y.values[i], gridCoords.X.values[i]),
+                       (gridCoords.Y.values[i+1], gridCoords.X.values[i+1]),
+                       (gridCoords.Y.values[i+2], gridCoords.X.values[i+2]),
+                       (gridCoords.Y.values[i+3], gridCoords.X.values[i+3]),
+                       (gridCoords.Y.values[i+4], gridCoords.X.values[i+4]))
+        # Append the poly_coords to a list for later use
+        gridLayout_Coords.append(poly_coords)
+
+        # # Getting the center points for the polygons and assigning them accordingly to the grid blocks
+        # poly = Polygon(poly_coords)
+        # center = poly.centroid.coords
+        # # The center coordinates for the current grid block
+        # center_lat = center[0][0]  # Latitude
+        # center_long = center[0][1]  # Longitude
+        # gridCoords.Center_Lat.values[i] = center_lat
+        # gridCoords.Center_Long.values[i] = center_long
 
         # Drawing the ghost grid blocks #
         # ghost_lats, ghost_longs = zip(*[(gridCoords.Ghost_Lat.values[i], gridCoords.Ghost_Long.values[i]),
@@ -86,52 +96,54 @@ for i, value in enumerate(gridCoords.values):
         #                                 (gridCoords.Ghost_Lat.values[i+3], gridCoords.Ghost_Long.values[i+3]),
         #                                 (gridCoords.Ghost_Lat.values[i+4], gridCoords.Ghost_Long.values[i+4])])
         # gmap.plot(ghost_lats, ghost_longs, 'white', edge_width=2)
-        # Making the actual ghost polygons using the ghost coordinates above for calculations and manipulations #
+        # # Making the actual ghost polygons using the ghost coordinates above for calculations and manipulations #
         # ghost_coords = ((gridCoords.Ghost_Lat.values[i], gridCoords.Ghost_Long.values[i]),
         #                 (gridCoords.Ghost_Lat.values[i+1], gridCoords.Ghost_Long.values[i+1]),
         #                 (gridCoords.Ghost_Lat.values[i+2], gridCoords.Ghost_Long.values[i+2]),
         #                 (gridCoords.Ghost_Lat.values[i+3], gridCoords.Ghost_Long.values[i+3]),
         #                 (gridCoords.Ghost_Lat.values[i+4], gridCoords.Ghost_Long.values[i+4]))
-        # Append the poly_coords to a list for later use #
+        # # Append the poly_coords to a list for later use #
         # ghostLayout_Coords.append(ghost_coords)
+# This is for saving any changes to the grid coordiantes file
+# gridCoords.to_csv("../Excel & CSV Sheets/Grid Oriented Small Layout Test Files/Vertices OS Layout.csv")
 
 # This code was used for initially finding the coordinates for the ghost grid blocks #
         # Finding the ghost coordinates for the ghost grid
         # Get the center coordinates for the current grid block's polygon
-        # poly = Polygon(poly_coords)
-        # center = poly.centroid.coords
-        # The center coordinates for the current grid block
-        # center_lat = center[0][0]  # Latitude
-        # center_long = center[0][1]  # Longitude
-        # Place a marker on the map for each center point (helps with visualizing ghost grid)
-        # gmap.marker(center_lat, center_long, '#FF0000', title=i)
-        # ghost_grid_lats, ghost_grid_longs = find_ghost(grid_lats, grid_longs, center_lat, center_long)
-        # Append the newly made ghost coordinates to the main vertices points file for future use
-        # I know this isn't the prettiest code, but forget your opinion 'cause it works
+#         poly = Polygon(poly_coords)
+#         center = poly.centroid.coords
+#         # The center coordinates for the current grid block
+#         center_lat = center[0][0]  # Latitude
+#         center_long = center[0][1]  # Longitude
+#         # Place a marker on the map for each center point (helps with visualizing ghost grid)
+#         gmap.marker(center_lat, center_long, '#FF0000', title=i)
+#         ghost_grid_lats, ghost_grid_longs = find_ghost(grid_lats, grid_longs, center_lat, center_long)
+#         # Append the newly made ghost coordinates to the main vertices points file for future use
+#         # I know this isn't the prettiest code, but forget your opinion 'cause it works
 #         gridCoords.Ghost_Lat.values[i], gridCoords.Ghost_Long.values[i] = ghost_grid_lats[0], ghost_grid_longs[0]
 #         gridCoords.Ghost_Lat.values[i+1], gridCoords.Ghost_Long.values[i+1] = ghost_grid_lats[1], ghost_grid_longs[1]
 #         gridCoords.Ghost_Lat.values[i+2], gridCoords.Ghost_Long.values[i+2] = ghost_grid_lats[2], ghost_grid_longs[2]
 #         gridCoords.Ghost_Lat.values[i+3], gridCoords.Ghost_Long.values[i+3] = ghost_grid_lats[3], ghost_grid_longs[3]
 #         gridCoords.Ghost_Lat.values[i+4], gridCoords.Ghost_Long.values[i+4] = ghost_grid_lats[4], ghost_grid_longs[4]
-# gridCoords.to_csv("../Excel & CSV Sheets/Grid Layout Test Files/VerticesPoints 2.csv")
+# gridCoords.to_csv("../")
         
 
 # Checking to see what calldata records each grid block has #
-# calldata = pandas.read_csv("../Excel & CSV Sheets/New Data Files/New Negative Samples (Location).csv", sep=",")
-# for j in range(0, len(gridLayout_Coords)):
-#     poly = Polygon(gridLayout_Coords[j])  # This is the grid block for the current iteration
-#     print(j)
-#     # take in the 911 incident lat and long one at a time
-#     for o, value2 in enumerate(calldata.values):
-#         call_lat = calldata.Latitude.values[o]
-#         call_long = calldata.Longitude.values[o]
-#         call_incident = Point(call_lat, call_long)
-#         # See if the 911 incident is in the current grid block polygon
-#         if poly.contains(call_incident):
-#             calldata.Grid_Block.values[o] = j
-#         else:
-#             pass
-# calldata.to_csv("../Excel & CSV Sheets/New Data Files/New Negative Samples (Location).csv")
+calldata = pandas.read_csv("../Excel & CSV Sheets/Grid Oriented Small Layout Test Files/Grid Oriented Small Data 2017+2018.csv", sep=",")
+for j in range(1, len(gridLayout_Coords)):
+    poly = Polygon(gridLayout_Coords[j])  # This is the grid block for the current iteration
+    print(j)
+    # take in the 911 incident lat and long one at a time
+    for o, value2 in enumerate(calldata.values):
+        call_lat = calldata.Latitude.values[o]
+        call_long = calldata.Longitude.values[o]
+        call_incident = Point(call_lat, call_long)
+        # See if the 911 incident is in the current grid block polygon
+        if poly.contains(call_incident):
+            calldata.Grid_Block.values[o] = j
+        else:
+            pass
+calldata.to_csv("../Excel & CSV Sheets/Grid Oriented Small Layout Test Files/Grid Oriented Small Data 2017+2018.csv")
 
 # Getting the center coordinate of each grid block #
 # These will be our weather points for each grid block
