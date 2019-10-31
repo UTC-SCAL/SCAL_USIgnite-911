a25d6098e6f22360473d676a846b24fb267a0ac7
JeremiahRoland <roland 101>
2019-10-31 12:45:38 -0400

    Finally updating the 2019 weather

--- Code/updatingWeatherFiles.py
+++ Code/updatingWeatherFiles.py
@@ -2,7 +2,7 @@ import pandas
 import os, sys
 from datetime import datetime, timedelta
 from darksky import forecast
-# import feather
+import feather
 
 
 def find_cred(service):
@@ -24,48 +24,64 @@ def fetchWeather(weatherFile, beginDate, endDate):
     print("Fetching Weather")
     dates = pandas.date_range(beginDate, endDate)
     # Read in the center points file to get the used grid blocks (ORIG_FID) and their lat/longs
-    centers = pandas.read_csv("../Excel & CSV Sheets/Grid Files/Grid Oriented Layout/CenterPoints Ori Layout.csv")
-    miss_loc = len(weatherFile)
+    centers = pandas.read_csv("../Excel & CSV Sheets/Hamilton County Accident System/Grid Files/Grid Oriented Layout/CenterPoints Ori Layout.csv")
+    # miss_loc = len(weatherFile)
+    miss_loc = 0
+    # crash point is used for resuming the code from when it was stopped, it refers
+    # to the grid block the code last fetched weather for
+    crash_point = 470
+    # stop point is used for stopping the code when it hits a certain grid block
+    # mainly used for parallelizing the code with other files
+    # only needs to be used for loops that don't go to the end of the grid blocks
+    stop_point = 600
     key = find_cred("darksky")
     for _, i in enumerate(centers.ORIG_FID.values):
         # This first loop is used to resume the code where it crashes, as sometimes darksky decides to deny us
-        # if i < 0:
-        #     print("Passing til ", i)
-        #     pass
-        # else:
-        i = int(i)
-        print(i)
-        lat = centers.Center_Lat.values[i]
-        long = centers.Center_Long.values[i]
-        for j in dates:
-            j = str(j)
-            j = str(j.split(" ")[0])
-            year = int(j.split("-")[0])
-            month = int(j.split("-")[1])
-            day = int(j.split("-")[2])
-            t = datetime(year, month, day, 0, 0, 0).isoformat()
-            call = key, lat, long
-            forecastcall = forecast(*call, time=t)
-            hourly_list = [item._data for item in forecastcall.hourly]
-            for l, info in enumerate(hourly_list):
-                entry = hourly_list[l]
-                for d, things in enumerate(hourly_list[l]):
-                    weatherFile.loc[miss_loc, things] = entry[things]
-                weatherFile.loc[miss_loc, "Center_Lat"] = lat
-                weatherFile.loc[miss_loc, "Center_Long"] = long
-                weatherFile.loc[miss_loc, "ORIG_FID"] = i
-                miss_loc += 1
-        return weatherFile
+        if i < crash_point + 1:
+            print("Passing til ", crash_point)
+            pass
+        elif i > stop_point:
+            print("Reached stopping point for this set of blocks, which is ", stop_point)
+            break
+        else:
+            i = int(i)
+            print(i)
+            lat = centers.Center_Lat.values[i]
+            long = centers.Center_Long.values[i]
+            for j in dates:
+                j = str(j)
+                j = str(j.split(" ")[0])
+                year = int(j.split("-")[0])
+                month = int(j.split("-")[1])
+                day = int(j.split("-")[2])
+                t = datetime(year, month, day, 0, 0, 0).isoformat()
+                call = key, lat, long
+                forecastcall = forecast(*call, time=t)
+                hourly_list = [item._data for item in forecastcall.hourly]
+                for l, info in enumerate(hourly_list):
+                    entry = hourly_list[l]
+                    for d, things in enumerate(hourly_list[l]):
+                        weatherFile.at[miss_loc, things] = entry[things]
+                    weatherFile.at[miss_loc, "Center_Lat"] = lat
+                    weatherFile.at[miss_loc, "Center_Long"] = long
+                    weatherFile.at[miss_loc, "ORIG_FID"] = i
+                    miss_loc += 1
+            if i % 10 == 0:
+                feather.write_dataframe(weatherFile, "../Ignore/Weather/2019 Weather File Update part 4.feather")
+    return weatherFile
 
 # Converting the unix time (labeled as time because Pete doesn't like consistency) to Date and timereadable
 def standardFromUnix(weatherFile):
     print("Converting Time")
+    fileLength = len(weatherFile.time.values)
     weatherFile.timereadable = weatherFile.timereadable.astype(str)
     weatherFile.Date = weatherFile.Date.astype(str)
     weatherFile.time = weatherFile.time.astype(int)
+    weatherFile.Unix = weatherFile.time.astype(int)
+    weatherFile.Grid_Block = weatherFile.ORIG_FID.astype(int)
 
     for i, value in enumerate(weatherFile.values):
-        print(i)
+        print(i, " / ", fileLength)
         unix = weatherFile.time.values[i]
         readTime = str(datetime.fromtimestamp(unix).strftime('%Y-%m-%d %H:%M:%S'))
         weatherFile.timereadable.values[i] = readTime.split(" ")[1]
@@ -92,12 +108,14 @@ def finding_binaries(weatherFile):
 def appendMainWeather(main_weather, new_weather):
     # Now, append the new weather file to the main weather file for the corresponding year #
     # This is the list order for the columns we want the weather files to have
-    header_list = ('Center_Lat', 'Center_Long', 'ORIG_FID', 'apparentTemperature', 'cloudCover', 'dewPoint', 'humidity',
-                   'Event', 'ozone', 'precipAccumulation', 'precipIntensity', 'precipProbability', 'precipType',
-                   'pressure',
-                   'Conditions', 'temperature', 'time', 'Date', 'timereadable', 'uvIndex', 'visibility', 'windBearing',
-                   'windGust', 'windSpeed', 'EventBefore', 'ConditionBefore', 'hourbefore', 'Rain', 'Cloudy', 'Foggy',
-                   'Snow', 'Clear')
+    header_list = ('Center_Lat', 'Center_Long', 'ORIG_FID',
+       'apparentTemperature', 'cloudCover', 'dewPoint', 'humidity', 'ozone',
+       'precipAccumulation', 'precipIntensity', 'precipProbability',
+       'precipType', 'pressure', 'temperature', 'Unix', 'Date', 'timereadable',
+       'uvIndex', 'visibility', 'windBearing', 'windGust', 'windSpeed',
+       'Event', 'Conditions', 'EventBefore', 'ConditionBefore', 'hourbefore',
+       'Grid_Block', 'Rain', 'Cloudy', 'Foggy', 'Snow', 'Clear', 'HourBefore',
+       'RainBefore')
     main_weather = main_weather.reindex(columns=header_list)
     new_weather = new_weather.reindex(columns=header_list)
     main_weather.timereadable = main_weather.timereadable.astype(str)
@@ -118,35 +136,42 @@ def appendMainWeather(main_weather, new_weather):
 
 # Use these lines for adding weather #
 # This file is only for reading in the columns we need for the weather file
-# weather_2019 = feather.read_dataframe("../Ignore/Weather/2019 Weather.feather")
-# weatherFile = pandas.DataFrame(columns=weather_2019.columns)
+# weather_columns = feather.read_dataframe("../Ignore/Weather/2019 Weather.feather")
+# print(weather_columns.columns)
+# weatherFile = pandas.DataFrame(columns=weather_columns.columns)
 # Set the start and end date for the weather file to be updated
-# begin = '2019-06-26'
-# end = '2019-09-05'
-# fetchWeather(weatherFile, begin, end)
+# begin = ''
+# end = ''
+# fetchedWeather = fetchWeather(weatherFile, begin, end)
+# feather.write_dataframe(fetchedWeather, "../")
+# fetchedWeather.to_csv("../")
 
+# Now, let's test to make sure we got the stuff we wanted
+# fetchedWeather = feather.read_dataframe("../")
+# print(fetchedWeather.Grid_Block.values[0])
+# print(fetchedWeather.Grid_Block.values[len(fetchedWeather.Unix.values)-1])
 
 # Use these lines for formatting the newly added weather #
-# weatherFile = pandas.read_csv("../Ignore/Weather/2019 Weather Updated4.csv")
 # Convert unix time to date and time readable
-# standardFromUnix(weatherFile)
+# fetchedWeather = standardFromUnix(fetchedWeather)
 # Get the aggregated values of event and conditions
-# finding_binaries(weatherFile)
-# weatherFile.to_csv("../Ignore/Weather/2019 Weather Updated4 Full.csv")
+# fetchedWeather = finding_binaries(fetchedWeather)
+# feather.write_dataframe(fetchedWeather, "../")
 
 # Use these lines to combine weather files #
 # Based on how many different files were made when getting new weather, add or remove weather# files to be appended
-# weather1 = pandas.read_csv("../")
-# weather2 = pandas.read_csv("../")
-# weather3 = pandas.read_csv("../")
-# weather4 = pandas.read_csv("../")
+# weather1 = feather.read_dataframe("../")
+# weather2 = feather.read_dataframe("../")
+# weather3 = feather.read_dataframe("../")
+
 
 # Append that shiz
-# new_weather = pandas.concat([weather1, weather2, weather3, weather4], axis=0, join='outer', ignore_index=False)
+# new_weather = pandas.concat([weather1, weather2, weather3], axis=0, join='outer', ignore_index=False)
 # Drop duplicates if there are any
 # new_weather.drop_duplicates(keep="first", inplace=True)
 # Save the new weather file if you want to
-# feather.write_dataframe(new_weather, "../")
+# feather.write_dataframe(new_weather, "../Ignore/Weather/2019 Weather Update Main.feather")
+
 # Read in the main weather file you'll be appending to
 # new_weather = feather.read_dataframe("../")
 # main_weather = feather.read_dataframe("../")
