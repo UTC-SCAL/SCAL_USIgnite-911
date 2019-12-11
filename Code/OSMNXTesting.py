9cc77056d2cf281dc6a86d5bffb6c15a60479d58
PeteWay <35679240+PeteWay@users.noreply.github.com>
2019-12-11 12:02:04 -0500

    After transparent bg work

--- Code/OSMNXTesting.py
+++ Code/OSMNXTesting.py
@@ -1,14 +1,25 @@
 import osmnx as ox
+import geopandas
+import matplotlib.pyplot as plt
 # G = ox.graph_from_place('Chattanooga, Tennessee, USA', network_type='drive')
 # ox.plot_graph(G)
+# import fiona
+# shape = fiona.open("/Users/peteway/Downloads/Chattanooga_Roads_City_Limits/Roads_Chat_City_Limits.shp")
+shape = geopandas.read_file("/Users/peteway/Downloads/Chattanooga_Roads_City_Limits/Roads_Chat_City_Limits.shp")
+print(shape.head())
+print(shape.columns.values)
+print(type(shape))
 
+fig, ax = plt.subplots(figsize = (10,10))
+shape.plot(ax=ax)
+plt.show()
 
-G2 = ox.graph_from_place('Isle of Wight, UK', network_type='drive')
-# ox.plot_graph(G2)
+# G2 = ox.graph_from_place('Isle of Wight, UK', network_type='drive')
+# # ox.plot_graph(G2)
 
-basic_stats = ox.basic_stats(G2)
-print(basic_stats)
-# print(basic_stats['circuity_avg'])
+# basic_stats = ox.basic_stats(G2)
+# print(basic_stats)
+# # print(basic_stats['circuity_avg'])
 
-extended_stats = ox.extended_stats(G2)
-print(extended_stats)
+# extended_stats = ox.extended_stats(G2)
+# print(extended_stats)
--- Code/testing.py
+++ Code/testing.py
@@ -3,29 +3,7 @@
 
 import pandas
 
-acc = pandas.read_csv("Excel & CSV Sheets/Accidents/RawAccidentData_DropDupsTest.csv")
-print(acc.columns)
 
-for i,info in enumerate(acc.values):
-    if acc.Latitude.values[i] > 36: 
-        acc.Latitude.values[i] = acc.Latitude.values[i]/1000000
-        acc.Longitude.values[i] = acc.Latitude.values[i]/-1000000
-acc.to_csv("Excel & CSV Sheets/Accidents/RawAccidentData_DropDupsTest2.csv", index=False)
-
-
-HCS-length(x,y):
-    m = x.length
-    n = y.length
-    let b[1...m,1..n] and c[0...m,0...n] be new tables
-    for i=1 to m:
-        c[i,0] = 0
-    for j=0 to n:
-        c[0,j] = 0
-    for i=1 to m:
-        for j=1 to n: 
-            if xi == y:
-                if (xi == A) or (xi == B)
-                    c[i,j] = c[i-1,j-1]+2;b[i,j]=" "
 
 
 
