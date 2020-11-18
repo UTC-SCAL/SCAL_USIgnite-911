0f773d88ca317e76e737d62d7d62d7fb29b1717f
miste <misternobrows@gmail.com>
2020-11-18 13:17:40 -0500

    Fetched accidents up to 11-18-2020 and formatted them to fit with our other raw accidents. Also added some comments to GettingRawData.py and New_Drop_Duplicates.py, which are the two files used to fetch and clean the accident record emails.

--- Code/New_Drop_Duplicates.py
+++ Code/New_Drop_Duplicates.py
@@ -1,47 +1,45 @@
+"""
+Author: Pete Way
+Editor: Jeremy Roland
+Purpose: Clean newly fetched accident files of potentially duplicate call records
+"""
 import pandas
-import numpy
 import time
-import math
 import geopy.distance
-import swifter
-from datetime import datetime
 
+# The file containing the newly fetched accident records
+fetchedAccidents = pandas.read_csv("../Excel & CSV Sheets/Grid Hex Layout/Accidents/RawAccidentData_NewFetch.csv")
+# The file containing our cleaned list of raw accident records
+cleanedAccidents = pandas.read_csv("../Excel & CSV Sheets/Grid Hex Layout/Accidents/RawAccidentData.csv")
+# Gets the date of the last record in our raw accident data
+lastcleaned = pandas.Timestamp(cleanedAccidents['Response Date'].values[-1]).date()
 
-accidents = pandas.read_csv("Excel & CSV Sheets/Grid Hex Layout/Accidents/RawAccidentData.csv")
+fetchedAccidents['Date'] = fetchedAccidents.apply(lambda x : pandas.Timestamp(x['Response Date']).date(), axis=1)
+fetchedAccidents['Hour'] = fetchedAccidents.apply(lambda x : pandas.Timestamp(x['Response Date']).hour, axis=1)
+fetchedAccidents['Coords'] = fetchedAccidents['Latitude'].astype(str) + " , " + fetchedAccidents['Longitude'].astype(str)
 
-cleaned = pandas.read_csv("Excel & CSV Sheets/Grid Hex Layout/Accidents/RawAccidentData_DropDups.csv")
-
-lastcleaned = pandas.Timestamp(cleaned['Response Date'].values[-1]).date()
-
-accidents['Date'] = accidents.apply(lambda x : pandas.Timestamp(x['Response Date']).date(), axis=1)
-accidents['Hour'] = accidents.apply(lambda x : pandas.Timestamp(x['Response Date']).hour, axis=1)
-accidents['Coords'] = accidents['Latitude'].astype(str) + " , " +accidents['Longitude'].astype(str)
-
-start = time.time()
 drops = list()
-for i, _ in enumerate(accidents.values):
-    if accidents.Date.values[i] >= lastcleaned:
-        if i % 2000 == 0: 
-            print(i, round(((time.time()-start)/60),2), len(drops))
-        timematches = accidents.loc[(accidents['Unix'].between((int(accidents.Unix[i]) - 900),(int(accidents.Unix[i]) + 900)))].index.tolist()
+for i, _ in enumerate(fetchedAccidents.values):
+    if fetchedAccidents.Date.values[i] >= lastcleaned:
+        # if i % 2000 == 0:
+        #     print(i, round(((time.time()-start)/60),2), len(drops))
+        timematches = fetchedAccidents.loc[(fetchedAccidents['Unix'].between((int(fetchedAccidents.Unix[i]) - 900),
+                                                                (int(fetchedAccidents.Unix[i]) + 900)))].index.tolist()
         if len(timematches) > 1:
             for j in timematches:
-                dist = geopy.distance.distance(accidents.Coords[i], accidents.Coords[j]).miles
-                if dist < .25 and (int(i) != int(j)) and j not in drops and (j>i):
+                dist = geopy.distance.distance(fetchedAccidents.Coords[i], fetchedAccidents.Coords[j]).miles
+                if dist < .25 and (int(i) != int(j)) and j not in drops and (j > i):
                     drops.append(j)
-keeps = accidents.drop(drops)
-end = time.time()
+keeps = fetchedAccidents.drop(drops)
 keeps = keeps.drop(['Coords', 'OK'], axis=1)
 
-##Getting the hour/date combo of the unix time here to avoid any missed duplicates. 
-keeps['Unix'] = keeps.apply(lambda x : pandas.datetime.strptime(str(x.Date) + " " + str(x.Hour).zfill(2), "%Y-%m-%d %H"), axis=1)
-keeps['Unix'] = keeps.apply(lambda x : x.Unix.strftime('%s'), axis=1)
+# Getting the hour/date combo of the unix time here to avoid any missed duplicates.
+keeps['Unix'] = keeps.apply(lambda x: pandas.datetime.strptime(str(x.Date) + " " +
+                                                               str(x.Hour).zfill(2), "%Y-%m-%d %H"), axis=1)
+# Depending on your OS, choose one of the following lines
+# keeps['Unix'] = keeps.apply(lambda x: x.Unix.strftime('%s'), axis=1)  # For Unix or Mac
+keeps['Unix'] = keeps.apply(lambda x: x.Unix.timestamp(), axis=1)  # For Windows
 
-print("Time taken:",round(((end-start)/60),2))
-print("Total Accidents Begin", len(accidents.values))
-print("Total Accidents End", len(keeps.values))
-print("Difference of:", int(len(accidents.values)-len(keeps.values)), "and percent of:",\
-     round(100*len(keeps.values)/len(accidents.values),2))
-
-
-keeps.to_csv("Excel & CSV Sheets/Grid Hex Layout/Accidents/RawAccidentData_DropDupsTest.csv", index=False)
+print("Duplicates Removed:", int(len(fetchedAccidents.values) - len(keeps.values)))
+# Save the dropped duplicates version
+keeps.to_csv("../Excel & CSV Sheets/Grid Hex Layout/Accidents/RawAccidentData_DropDupsTest.csv", index=False)
